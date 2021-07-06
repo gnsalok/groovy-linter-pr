339aac62ab8a02496a939161c706bde98cd5a2a1
aloktr <aloktr@cisco.com>
2021-07-06 10:25:51 +0530

    updating class

---
+++ src/class.groovy
@@ -0,0 +1,12 @@
+class Student {
+   int StudentID;
+   String StudentName;
+	
+   static void main(String[] args) {
+      Student st = new Student();
+      st.StudentID = 1;
+      st.StudentName = "Joe  
+
+      print(st)
+   } 
+}
